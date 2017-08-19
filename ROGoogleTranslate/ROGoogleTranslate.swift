e92e464b5561c7296375341d379c3be6ce1e66ee
Lung <chunlungfung@gmail.com>
2017-08-19 12:13:22 +0800

    callback for error

--- ROGoogleTranslate/ROGoogleTranslate.swift
+++ ROGoogleTranslate/ROGoogleTranslate.swift
@@ -39,17 +39,19 @@ open class ROGoogleTranslate {
     /// - parameter params:   ROGoogleTranslate Struct contains all the needed parameters to translate with the Google Translate API
     /// - parameter callback: The translated string will be returned in the callback
     ///
-    open func translate(params: ROGoogleTranslateParams, callback: @escaping (_ translatedText: String) -> ()) {
+    open func translate(params: ROGoogleTranslateParams, callback: @escaping (_ translatedText: String?, _ error: Error?) -> ()) {
 
         guard
             let urlEncodedText = params.text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
             let url            = URL(string: "https://www.googleapis.com/language/translate/v2?key=\(self.apiKey)&q=\(urlEncodedText)&source=\(params.source)&target=\(params.target)") else {
+                callback(nil, nil)
                 return
         }
         
         let httprequest = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
             
             guard error == nil, (response as? HTTPURLResponse)?.statusCode == 200 else {
+                callback(nil, error)
                 print("Something went wrong: \(String(describing: error?.localizedDescription))")
                 return
             }
@@ -65,9 +67,10 @@ open class ROGoogleTranslate {
                     else {
                         return
                 }
-                callback(translatedText)
+                callback(translatedText, nil)
                 
             } catch {
+                callback(nil, error)
                 print("Serialization failed: \(error.localizedDescription)")
             }
         })
