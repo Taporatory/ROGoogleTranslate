218ed1649d37023bfc9efcc6e70235167042b278
Lung <chunlungfung@gmail.com>
2017-08-23 16:26:50 +0800

    callback for error

--- ROGoogleTranslate/ROGoogleTranslate.swift
+++ ROGoogleTranslate/ROGoogleTranslate.swift
@@ -40,10 +40,13 @@ open class ROGoogleTranslate {
     /// - parameter callback: The translated string will be returned in the callback
     ///
     open func translate(params: ROGoogleTranslateParams, callback: @escaping (_ translatedText: String?, _ error: Error?) -> ()) {
-
+        
+//        print("params")
         guard
             let urlEncodedText = params.text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-            let url            = URL(string: "https://www.googleapis.com/language/translate/v2?key=\(self.apiKey)&q=\(urlEncodedText)&source=\(params.source)&target=\(params.target)") else {
+//            let url            = URL(string: "https://translate.google.com/translate_a/single?client=gtx&sl=" + params.source + "&tl=" + params.target + "&dt=t&q=" + urlEncodedText)
+            let url            = URL(string: "https://www.googleapis.com/language/translate/v2?key=\(self.apiKey)&q=\(urlEncodedText)&source=\(params.source)&target=\(params.target)")
+        else {
                 callback(nil, nil)
                 return
         }
@@ -57,6 +60,11 @@ open class ROGoogleTranslate {
             }
             
             do {
+//                if let data            = data {
+//                    let json            = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
+//                    callback(nil, nil)
+//                    return
+//                }
                 guard
                     let data            = data,
                     let json            = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary,
@@ -65,6 +73,7 @@ open class ROGoogleTranslate {
                     let translation     = translations.first            as? [String : Any],
                     let translatedText  = translation["translatedText"] as? String
                     else {
+                        callback(nil, nil)
                         return
                 }
                 callback(translatedText, nil)
